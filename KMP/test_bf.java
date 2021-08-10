236dbe1240eb6b783f0ff90fc03baea5958844ec
sunbin <769460962@qq.com>
2021-08-10 16:21:20 +0800

    字符串暴力查找

--- KMP/test_bf.java
+++ KMP/test_bf.java
@@ -1,29 +1,39 @@
 
-/**
- * 暴力破解法
- * @param ts 主串
- * @param ps 模式串
- * @return 如果找到，返回在主串中第一个字符出现的下标，否则为-1
- */
-public static int bf(String ts, String ps) {
-    char[] t = ts.toCharArray();
-    char[] p = ps.toCharArray();
-    int i = 0; // 主串的位置
-    int j = 0; // 模式串的位置
+class TestBf {
+    /**
+     * 暴力破解法
+     * @param ts 主串
+     * @param ps 模式串
+     * @return 如果找到，返回在主串中第一个字符出现的下标，否则为-1
+     */
+    public static int bf(String ts, String ps) {
+        char[] t = ts.toCharArray();
+        char[] p = ps.toCharArray();
+        int i = 0; // 主串的位置
+        int j = 0; // 模式串的位置
 
-    while (i < t.length && j < p.length) {
-       if (t[i] == p[j]) { // 当两个字符相同，就比较下一个
-           i++;
-           j++;
-       } else {
-           i = i - j + 1; // 一旦不匹配，i后退
-           j = 0; // j归0
-       }
+        while (i < t.length && j < p.length) {
+           if (t[i] == p[j]) { // 当两个字符相同，就比较下一个
+               i++;
+               j++;
+           } else {
+               i = i - j + 1; // 一旦不匹配，i后退
+               j = 0; // j归0
+           }
+        }
+
+        if (j == p.length) {
+           return i - j;
+        } else {
+           return -1;
+        }
     }
 
-    if (j == p.length) {
-       return i - j;
-    } else {
-       return -1;
+    public static void main(String[] args) {
+        String ts = "SSSSSSSSSSSSSA";
+        String ps = "SSSSA";
+        int pos = bf(ts, ps);
+        System.out.println("pos=" + pos);
     }
 }
+
